0f6efe03956cf01e7c849cb6d4cc1a0f987dd1f0
KSksip <->
2025-11-21 10:21:11 +0100

    nitro init

---
+++ nitro.config.ts
@@ -0,0 +1,12 @@
+import { defineNitroConfig } from "nitropack/config"
+
+// https://nitro.build/config
+export default defineNitroConfig({
+  compatibilityDate: "latest",
+  srcDir: "server",
+  imports: false,
+  serveStatic: true,
+  experimental: {
+    websocket: true
+  }
+});
