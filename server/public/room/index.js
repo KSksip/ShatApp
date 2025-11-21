8e4b18671877a6e6536040bb41b3d1d638de311d
KSksip <->
2025-11-21 13:56:51 +0100

    added xss prevention and clear chat button aswell as som QoL improvements

--- server/public/room/index.js
+++ server/public/room/index.js
@@ -1,9 +1,10 @@
-const socket = new WebSocket('ws://localhost:3000/websocket/room')
+const socket = new WebSocket('/websocket/room')
 
 const nicknameEl = document.getElementById('nicknameDisplay')
 const sendBtn = document.getElementById('sendBtn')
 const messageInput = document.getElementById('messageInput')
 const chatField = document.getElementById('chatField')
+const clearChatBtn =document.getElementById('clearChatBtn')
 
 let nickname = localStorage.getItem('nickname')
 let messageLog = { messages: [] }
@@ -19,8 +20,6 @@ if(localStorage.getItem('messageLog')){
         renderMessage(message)
     }
 }
-
-
 
 nicknameEl.innerText = nickname
 
@@ -43,20 +42,41 @@ socket.onmessage = async (event) => {
 
 console.log('rans')
 
+clearChatBtn.addEventListener('click', clearChat)
+sendBtn.addEventListener('click', sendMessage)
 
-sendBtn.addEventListener('click', () => {
+messageInput.addEventListener('keyup', (event) => {
+    if(event.key == 'Enter'){
+        sendMessage()
+    }
+})
+
+function sanitizeHTML(str) {
+    if(str){
+        return str.replace(/[^\w. ]/gi, function (c) {
+            return '&#' + c.charCodeAt(0) + ';';
+        });
+        
+    }
+};
+
+function clearChat(){
+    localStorage.setItem('messageLog', '')
+    chatField.innerHTML = ""
+}
+
+function sendMessage(){
     const message = {
         nickname: nickname,
         timeStamp: String(new Date),
         data: messageInput.value
     }
+    messageInput.value = ""
     renderMessage(message)
-    
     const data = JSON.stringify(message)
     saveMessage(data)
     socket.send(data)
-})
-
+}
 
 function saveMessage(message){
     console.log(message)
@@ -69,14 +89,15 @@ function saveMessage(message){
 function renderMessage(message){
     chatField.innerHTML += `
         <div class="flex-h" style="gap: 0.7rem;">
-            <h4>${message.nickname}</h4>
+            <h4>${sanitizeHTML(message.nickname)}</h4>
             <p> • </p>
-            <span>${message.timeStamp}</span>
+            <span>${sanitizeHTML(message.timeStamp)}</span>
         </div>
     
         <div style="padding: 0 0 0.5rem 0.5rem; ">
-            <p>${message.data}</p>
+            <p>${sanitizeHTML(message.data)}</p>
         </div>
         <hr>
     `
+    chatField.scrollTop = chatField.scrollHeight
 }
